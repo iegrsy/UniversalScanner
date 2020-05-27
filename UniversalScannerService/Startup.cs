a8bbb7aa68cd147f595acbf17e4a06b389ceb19f
Ibrahim Ethem Gursoy <ibrahim.gursoy@sparsetechnology.com>
2020-05-27 18:11:12 +0300

    Show found device

--- UniversalScannerService/Startup.cs
+++ UniversalScannerService/Startup.cs
@@ -55,7 +55,7 @@ namespace UniversalScannerService
                 endpoints.MapGrpcReflectionService();
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                    await context.Response.WriteAsync($"{UScannerServiceImpl.FoundDeviceList}");
                 });
             });
         }
